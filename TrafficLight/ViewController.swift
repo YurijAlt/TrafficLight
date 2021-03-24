881321a5b69b8644c54fdbc43d6905a18f5eb110
YurijAlt <67707910+YurijAlt@users.noreply.github.com>
2021-03-24 23:26:13 +0300

    Configured interface for iPhone 11 and code

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,25 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //MARK: Outlets
+    enum TrafficLightColors {
+        case red
+        case yellow
+        case green
+    }
+    
+    //MARK: IB Outlets
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
     
     @IBOutlet weak var startButton: UIButton!
     
+    //MARK: Public Properties
+    let colorTransperancy: CGFloat = 0.5
     
+    //MARK: Private Properties
+    private var brightLight = TrafficLightColors.red
+    
+    
+    //MARK: Life Cicles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+
         redLight.layer.cornerRadius = redLight.bounds.height / 2
+        redLight.alpha = colorTransperancy
         yellowLight.layer.cornerRadius = redLight.bounds.height / 2
+        yellowLight.alpha = colorTransperancy
         greenLight.layer.cornerRadius = redLight.bounds.height / 2
+        greenLight.alpha = colorTransperancy
+        
         startButton.layer.cornerRadius = 10
     }
-
     
+    //MARK: IB Actions
     @IBAction func startButtonTapped(_ sender: UIButton) {
+        
+        startButton.setTitle("NEXT", for: .normal)
+        
+        func changeColor(color: TrafficLightColors) {
+            
+            switch brightLight {
+            case .red:
+                redLight.alpha = 1
+                greenLight.alpha = colorTransperancy
+                brightLight = .yellow
+                
+            case .yellow:
+                redLight.alpha = colorTransperancy
+                yellowLight.alpha = 1
+                brightLight = .green
+            case .green:
+                yellowLight.alpha = colorTransperancy
+                greenLight.alpha = 1
+                brightLight = .red
+            }
+        }
+        changeColor(color: brightLight)
     }
-    
 }
 
